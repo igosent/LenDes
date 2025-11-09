f535603aa27893c73162bb01056349045ffe2a80
igosent <bojkoigor38@gmail.com>
2025-11-09 01:07:51 +0100

    laslalal

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   base: './', // важно для продакшн, чтобы CSS/JS грузились корректно
